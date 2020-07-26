0df44c278194fa6ebf6d47b79cfc6188e36655fa
alvarovs89 <67175098+alvarovs89@users.noreply.github.com>
2020-07-26 15:44:16 +1000

    Initial Commit

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Alvaro Valdes Salazar on 26-07-20.
+//  Copyright © 2020 Alvaro Valdes Salazar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
