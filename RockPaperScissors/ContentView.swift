7c4be9c35d59d11485b05f3bc110e22777524564
alvarovs89 <67175098+alvarovs89@users.noreply.github.com>
2020-07-27 22:45:11 +1000

    Primera version

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -9,10 +9,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let allChoices = ["Rock", "Paper", "Scissors"].shuffled()
+    @State private var iaAnwser = Int.random(in: 0...2)
+    @State private var userScore:Int = 0
+    @State private var showingScore = false
+
+    
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Your Score is: \(userScore)")
+            Spacer()
+            HStack {
+                ForEach (0..<3) { number in
+                    Button(action: {
+                            self.handTapped(number) // flag was tapped
+                    }) {
+                        Image(self.allChoices[number])
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width: 80, height: 80)
+                        
+                    }
+//            Spacer()
+
+                }
+
+            }
+            Spacer()
+    
+        }
+    }
+    func handTapped(_ number: Int) {
+        if number == 0 { // I CHOSE ROCK
+            if iaAnwser==1 {
+//            case 1: // IA CHOSE PAPER, I LOSE
+            userScore -= 1
+            }
+            if iaAnwser==2 {
+//            case 2: // IA CHOSE SCISSOR, I WIN
+            userScore += 1
+            }
+//            default: // IA CHOSE ROCK, WE TIE
+//                showingScore = true
+        }
     }
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
