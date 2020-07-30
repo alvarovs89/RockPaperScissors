7b1853222ef7f1357e87fd96b8ce26c9704c6075
alvarovs89 <67175098+alvarovs89@users.noreply.github.com>
2020-07-30 21:28:19 +1000

    casi final

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -16,7 +16,11 @@ extension Image {
             .renderingMode(.original)
             .resizable()
             .frame(width: 80, height: 80)
-   }
+            .cornerRadius(25)
+            .overlay(RoundedRectangle(cornerRadius: 25)
+             .stroke(Color.blue, lineWidth: 2))
+            
+    }
 }
 
 
@@ -24,127 +28,122 @@ struct ContentView: View {
     private let allChoices = ["Rock", "Paper", "Scissors"]
     @State private var iaAnwser = Int.random(in: 0...2)
     @State private var userScore:Int = 0
+    @State private var computerChoice:String = ""
+    @State private var myChoice:String = ""
+
     @State private var showingScore = false
-    @State private var mensajito:String = ""
+    @State private var resultGame:String = ""
     @State private var contador:Int = 0
 
-    private let titulo = """
-Rock Paper Scissors \n The game
-"""
+    private let title = "Rock Paper Scissors \n The game"
     
     var body: some View {
         VStack {
-            Text("\(titulo)")
+            Text("\(title)")
                 .font(.largeTitle)
                 .multilineTextAlignment(.center)
             Spacer()
-            if contador != 0 {
-            Text("The I.A. chose: \(allChoices[iaAnwser])")
-                Image(allChoices[iaAnwser])
-                    .myImageModifier()
-            } else {
-                Text("The I.A. haven't chose yet")
-
+            Group {
+                if contador != 0 {
+                Text("The I.A. chose: \(allChoices[iaAnwser])")
+                    Image(allChoices[iaAnwser])
+                        .myImageModifier()
+                } else {
+                    Text("The I.A. haven't chose yet")
+                }
             }
-
-
             Spacer()
-            Text(": \(mensajito)")
-
+            Text("You: \(resultGame)")
+                .font(.title)
 
             Text("Your Score is: \(userScore)")
+                .font(.title)
 
             Spacer()
             Text("Number of Games: \(contador)")
+            Spacer()
 
             HStack {
 
                 ForEach (0..<3) { number in
                     Button(action: {
-                            self.handTapped(number) // flag was tapped
+                            self.theGame(number) // flag was tapped
                         self.contador += 1
                     }) {
                         Image(self.allChoices[number])
                         .myImageModifier()
-                        
                     }
-//            Spacer()
-
                 }
-
             }
-    
         }
     }
-//        .alert(isPresented: $showingScore) {
-//    //            I add Text("\(scoreMessage)" to a message:
-//                Alert(title: Text(scoreTitle), message: Text("\(scoreMessage)"), dismissButton: .default(Text("Continue")) {
-//                    self.askQuestion()
-//                    })
-//            }
+    func theGame(_ number:Int) -> Void {
+        self.myChoice = self.allChoices[number]
+        iaAnwser = Int.random(in: 0..<self.allChoices.count)
+        self.computerChoice = self.allChoices[iaAnwser]
+        winOrLoose(myChoice, computerChoice)
+     
+        switch self.resultGame {
+        case "Win":
+            self.userScore += 1
+        case "Lose":
+            self.userScore -= 1
+        default:
+            return
+        }
+        
+    }
+    
+    func winOrLoose(_ user: String,_ computer: String) {
+        self.resultGame = ""
+        if user == computer {
+            self.resultGame = "Tie"
+        }
+        if(user == "Rock" && computer == "Scissors") {
+            self.resultGame = "Win"
+        }
+        if(user == "Scissors" && computer == "Paper") {
+            self.resultGame = "Win"
+        }
+        if(user == "Paper" && computer == "Rock") {
+            self.resultGame = "Win"
+        }
+        if(self.resultGame != "Win" && self.resultGame != "Tie") {
+            self.resultGame = "Lose"
+        }
+
+    }
+    
+
+//
+//        if iaAnwser==0 {
+//            userScore -= 1      // IA CHOSE SCISSOR, I LOSE
+//            mensajito = "You lose"
+//            iaAnwser = Int.random(in: 0...2)
 //        }
-
-    func handTapped(_ number: Int) {
-        if number == 0 { // I CHOSE ROCK
-            if iaAnwser==1 {
-            userScore -= 1       // IA CHOSE PAPER, I LOSE
-            mensajito = "You lose"
-            iaAnwser = Int.random(in: 0...2)
-            }
-            if iaAnwser==2 {
-            userScore += 1      // IA CHOSE SCISSOR, I WIN
-            mensajito = "You win"
-            iaAnwser = Int.random(in: 0...2)
-
-            } else {
-                mensajito = "It's a Tie"
-                iaAnwser = Int.random(in: 0...2)
-            }
-        }
-        if number == 1 { // I CHOSE PAPER
-                
-            if iaAnwser==0 {
-            userScore += 1       // IA CHOSE ROCK, I WIN
-            mensajito = "You win"
-                iaAnwser = Int.random(in: 0...2)
-            }
-            if iaAnwser==2 {
-            userScore -= 1      // IA CHOSE SCISSOR, I LOSE
-            mensajito = "You lose"
-                iaAnwser = Int.random(in: 0...2)
-            } else {
-                    mensajito = "It's a Tie"
-                    iaAnwser = Int.random(in: 0...2)
-            }
-        }
-    if number == 2 { // I CHOSE SCISSORS
-            
- 
-        if iaAnwser==0 {
-            userScore -= 1      // IA CHOSE SCISSOR, I LOSE
-            mensajito = "You lose"
-            iaAnwser = Int.random(in: 0...2)
-        }
-        if iaAnwser==1 {
-            userScore += 1       // IA CHOSE ROCK, I WIN
-            mensajito = "You win"
-             iaAnwser = Int.random(in: 0...2)
-         } else {
-            mensajito = "It's a Tie"
-            iaAnwser = Int.random(in: 0...2)
-        }
-    }
-    }
-    
-//    func askQuestion() {
-//        correctAnswer = Int.random(in: 0...2)
+//        if iaAnwser==1 {
+//            userScore += 1       // IA CHOSE ROCK, I WIN
+//            mensajito = "You win"
+//             iaAnwser = Int.random(in: 0...2)
+//         } else {
+//            mensajito = "It's a Tie"
+//            iaAnwser = Int.random(in: 0...2)
+//        }
 //    }
+//    }
+    
+
+    
 }
 
 
 
 struct ContentView_Previews: PreviewProvider {
    static var previews: some View {
-ContentView()
-   }
+    Group {
+        ContentView()
+        ContentView()
+            .preferredColorScheme(.dark)
+        }
+    }
 }
